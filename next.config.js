dbea039ca97d899efadf762c1571ab5acd16be53
April Wang from ETHZ <april.wang@inf.ethz.ch>
2025-03-15 17:51:59 +0100

    Deploy Next.js to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
       });
       return config;
     },
+    output: "export", // Required for `next export`
+    basePath: "/web-2025", // ⚠️ Must match your repo name
+    assetPrefix: "/web-2025/", // Ensures assets load correctly
+    images: {
+      unoptimized: true, // ⚠️ Required for GitHub Pages, since no Image Optimization
+    },
   };
   
   module.exports = nextConfig;
